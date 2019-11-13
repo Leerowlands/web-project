f7d055e9e89e6914958f6365f04786ce7ecb4f65
Leerowlands <leerow999@gmail.com>
2019-11-13 12:11:37 +0000

    change Jenkinsfile and node.js

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,12 +1,10 @@
 pipeline {
   agent any
- 
-  tools {nodejs "node"}
- 
-  stages {
-    stage('Example') {
-      steps {
-        sh 'npm config ls'
+   
+    stages {
+      stage('Build') {
+        steps {
+         'npm install'
       }
     }
   }
--- node.js
+++ node.js
@@ -1,10 +1,9 @@
 var http = require('http');
 var fs = require('fs');
-http.createServer(function (req, res){
-  fs.readFile('index.html', function(err, data){
-    res.writeHead(200, {'Content-Type' : 'html'});
+http.createServer(function (req, res) {
+  fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     res.end();
   });
-
-}).listen(3000);
+}).listen(4000);
